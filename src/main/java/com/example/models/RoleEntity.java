02827f2f13c71e6fa47c5f344f807d8eec5950eb
Adriel Alejandro Aliaga Benavides <adriel.aliaga@qa-bit.com>
2023-08-02 23:58:45 -0400

    Initial commit

---
+++ src/main/java/com/example/models/RoleEntity.java
@@ -0,0 +1,24 @@
+package com.example.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+}
