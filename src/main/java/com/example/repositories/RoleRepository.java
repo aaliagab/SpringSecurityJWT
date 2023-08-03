2732a11864c744ec9ae1d113ce9c638cd0419c2f
Adriel Alejandro Aliaga Benavides <adriel.aliaga@qa-bit.com>
2023-08-03 00:02:19 -0400

    Teminado entidades y repositorios

--- src/main/java/com/example/repositories/RoleRepository.java
+++ src/main/java/com/example/repositories/RoleRepository.java
@@ -1,9 +1,14 @@
 package com.example.repositories;
 
+import com.example.models.ERole;
 import com.example.models.RoleEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends CrudRepository<RoleEntity,Long> {
+
+    Optional<RoleEntity> findByName(ERole name);
 }
